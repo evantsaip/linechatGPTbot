27f6c97c0ad8b9c5e4624e8ef8864e868d8cdd5e
evan <evantsaip@gmail.com>
2023-03-31 19:15:44 +0800

    fix-typo

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const config = {
 }
 
 // openai key
-apiKey = process.env.OPENAI_API_KEY;
+apiKey2 = process.env.OPENAI_API_KEY;
 
 
 // create LINE SDK client
@@ -23,9 +23,43 @@ const app = express();
 app.post('/callback', line.middleware(config), (req, res) => {
     Promise
         .all(req.body.events.map(handleEvent))
-        .then((result) => res.json(result));
+        .then((result) => res.json(result))
         .catch((err) => {
             console.error(err);
             res.status(500).end();
         });
+});
+
+//event handler
+async function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text'){
+        return Promise.resolve(null);
+    }
+    //openai api chat3.5
+    const configuration = new Configuration({ apiKey: apiKey2 });
+    const openai = new OpenAIApi(configuration);
+
+    const openaiResponse = await openai.createChatCompletion({
+        //model type
+        model: 'gpt-3.5-turbo',
+
+        //message
+        messages: [ 
+            { role:'system',content: 'You are a helpful assistant.'  },
+            { role:'user',content: event.message.text  },
+         ],
+         //max tokens
+        maxTokens: 2000,
+        //temperature
+        temperature: 0.2,
+    });
+    //reply
+    const assistantReply = openaiResponse.data.choices[0].message.content;
+    const reply = { type: 'text', text: assistantReply };
+    return client.replyMessage(event.replyToken, reply);
+}
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
 });
